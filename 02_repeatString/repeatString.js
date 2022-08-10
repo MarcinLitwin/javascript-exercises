af2e650b542d2ba6166b304a95de375aab1f6df4
Maciej <m.rybkowski@icloud.com>
2022-08-10 18:56:56 +0200

    first and second task done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function (string, times) {
+    let outcomeString = [];
+    let ourString;
+    if (times > 0) {
+        for (let i = 0; i < times; i++) {
+            outcomeString.push(string);
+            ourString = outcomeString.join('');
+        }
 
+        return ourString;
+    } else if (times == 0) {
+        return '';
+    } else if (times < 0) {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
