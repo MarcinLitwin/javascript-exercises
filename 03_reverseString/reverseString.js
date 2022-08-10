f6703c24d96cb497908a83b85d2496d15863afbf
Maciej <m.rybkowski@icloud.com>
2022-08-10 19:09:15 +0200

    3rd task completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+    let stringLength = string.length;
+    let reversedString = [];
+    for (let i = 0; i < stringLength + 1; i++) {
+        reversedString.push(string.charAt(stringLength - i));
+    }
+    let stringFromOurArray = reversedString.join('');
+    return stringFromOurArray;
 };
 
 // Do not edit below this line
