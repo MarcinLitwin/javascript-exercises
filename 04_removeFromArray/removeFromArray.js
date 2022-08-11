8b20428115a7e01d63154c0b4be0e1b43d76a952
Maciej <m.rybkowski@icloud.com>
2022-08-11 15:35:34 +0200

    completed 4th task with large problems

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+    let numbers = Array.from(args);
+    let ourArray = array;
 
+    for (i = 0; i < numbers.length; i++) {
+        let number = numbers[i];
+        for (n = 0; n < ourArray.length; n++) {
+            if (number === ourArray[n]) {
+                ourArray.splice(n, 1);
+            }
+        }
+    }
+    return ourArray;
 };
 
 // Do not edit below this line
