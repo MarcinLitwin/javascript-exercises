0f051fb416f679805de4c693d7d745f730edcca4
Maciej <m.rybkowski@icloud.com>
2022-08-11 17:02:51 +0200

    7th task done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,21 @@
-const ftoc = function() {
-
+const ftoc = function (celc) {
+    if (typeof celc === 'number') {
+        return Math.round((celc - 32) * (5 / 9) * 10) / 10;
+    } else {
+        return 'ERROR';
+    }
 };
 
-const ctof = function() {
-
+const ctof = function (fahr) {
+    if (typeof fahr === 'number') {
+        return Math.round((fahr * (9 / 5) + 32) * 10) / 10;
+    } else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
 module.exports = {
-  ftoc,
-  ctof
+    ftoc,
+    ctof,
 };
