f36dace16cc6c182ad2383cdb5071758b71333c8
Maciej <m.rybkowski@icloud.com>
2022-08-11 16:50:52 +0200

    6th task done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+    let givenYear = Number(year);
+    if (typeof year === 'number') {
+        if (givenYear % 4 == 0) {
+            if (givenYear % 100 == 0 && givenYear % 400 != 0) {
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    } else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
