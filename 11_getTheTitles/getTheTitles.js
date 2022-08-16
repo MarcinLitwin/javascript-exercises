eebfada3f00fea0b5cbd0336a538c347f43b9104
Maciej <m.rybkowski@icloud.com>
2022-08-16 22:23:20 +0200

    11th task done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (array) {
+    const titles = [];
+    let ammountOfObjects = array.length;
+    for (i = 0; i < ammountOfObjects; i++) {
+        let currentObject = array[i];
+        titles.push(currentObject.title);
+    }
+    return titles;
 };
 
 // Do not edit below this line
