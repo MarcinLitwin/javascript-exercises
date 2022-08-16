d3322ae3599579c251b9d0f6974eb574f53e642d
Maciej <m.rybkowski@icloud.com>
2022-08-16 22:13:04 +0200

    completed 10th task

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function (number) {
+    if (Number(number) > 0) {
+        let result = 1;
+        let firstNumber = 1;
+        let secondNumber = 1;
+        for (let i = 1; i < Number(number) - 1; i++) {
+            result = firstNumber + secondNumber;
+            firstNumber = secondNumber;
+            secondNumber = result;
+        }
+        return result;
+    } else {
+        return 'OOPS';
+    }
 };
 
 // Do not edit below this line
