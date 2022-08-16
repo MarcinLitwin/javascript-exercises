97a4ff2650adaf3b2ffe2e669f54766f70a22dca
Maciej <m.rybkowski@icloud.com>
2022-08-16 22:55:00 +0200

    done 12th

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+    let objectWithOldestPerson;
+    let lifetime = 0;
+    let length = array.length;
+    let latestYear;
 
+    for (let i = 0; i < length; i++) {
+        let currentObject = array[i];
+        let currentLifetime =
+            currentObject.yearOfDeath - currentObject.yearOfBirth;
+        if (currentObject.yearOfDeath == undefined) {
+            currentLifetime = 2022 - currentObject.yearOfBirth;
+        }
+        if (currentLifetime > lifetime) {
+            lifetime = currentLifetime;
+            objectWithOldestPerson = currentObject;
+        } else {
+            continue;
+        }
+    }
+    return objectWithOldestPerson;
 };
 
 // Do not edit below this line
